74c4496b8f30498145baf7ff0cb844d9c2fe591e
Rodrigo Missagia Hulle <rmhulle@gmail.com>
2016-09-13 21:37:06 +0000

    validating articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ActiveRecord::Base
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  
 end
--- spec/features/article.rb
+++ spec/features/article.rb
@@ -10,11 +10,29 @@ RSpec.feature "Creating Articles" do
     fill_in "Title", with: "Creating the first Article"
     fill_in "Body", with: "Lorem Ipsum"
 
-    click_button "create Article"
+    click_button "Create Article"
 
     expect(page).to have_content("Article has been created")
     
     expect(page.current_path).to eq(articles_path)
+  end  
+  
+  
+  
+  scenario "User fails to create a new Article" do
+
+    visit "/"
+    click_link "New Article"
+    
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+    
   end  
   
   
